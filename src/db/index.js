b0219319d35f403b6b9d78663be88e6e3e7882be
Shravani Pujari <shravanipujari2513@gmail.com>
2025-09-08 16:59:26 +0530

    database connected to MERN

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB=async()=>{
+   try{
+      const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+      console.log(`MONGODB connected!! DB host:${connectionInstance.connection.host }`);
+   }
+catch(error){
+    console.log("MONGODB Connection Error",error);
+    throw error;
+    process.exit(1);
+}
+}
+
